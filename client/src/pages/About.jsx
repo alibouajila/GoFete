cf9925ee819599bb3353ff8ccc9ba38f7ea28d8b
Ali Bouajila <alibouajila18@gmail.com>
2025-03-04 09:53:27 +0100

    Fixing About us page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <p>
       Gofete is a dynamic platform that allows organizers to create and manage events while enabling users to browse and explore events of interest. Organizers can add event details, ensuring accurate and up-to-date information. Users can effortlessly navigate through events, view essential details, and stay informed about upcoming activities. With a user-friendly interface and smooth navigation, the app enhances event discovery and engagement.
       </p>
-      <p>ajjza kkzk kk
+      <p>
         Our team is committed to innovation and continuous improvement, ensuring that you always
         get the best features and support. Thank you for being a part of our journey!
       </p>
